1a2486264d1fbbdc4ce3d6a7acf2f7686101c5b2
KareemShaban1 <karim17196@fci.bu.edu.eg>
2025-09-24 23:35:36 +0300

    fix parameters issue

--- src/Console/Commands/ScanTranslations.php
+++ src/Console/Commands/ScanTranslations.php
@@ -13,9 +13,10 @@ class ScanTranslations extends Command
     {--locales= : Comma-separated locales, e.g. en,ar}
     {--translate : Enable auto translation via Google API (experimental)}
     {--ignore= : Comma-separated paths to ignore (e.g. views,controllers)}
-    {--ignoreFile= : Comma-separated file names to ignore (e.g. welcome.blade.php,home.blade.php)}
-    {--ignoreDir= : Comma-separated directories to ignore (e.g. vendor,storage,cache)}
+    {--ignore-file= : Comma-separated file names to ignore (e.g. welcome.blade.php,home.blade.php)}
+    {--ignore-dir= : Comma-separated directories to ignore (e.g. vendor,storage,cache)}
     {--overwrite : Overwrite existing translations with new ones}';
+
 
 
     protected $description = 'Scan Blade & PHP files recursively and update translation files';
