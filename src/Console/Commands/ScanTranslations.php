818b8f9c56fb584c2876c56e416cd02ad424102b
KareemShaban1 <karim17196@fci.bu.edu.eg>
2025-09-25 16:06:20 +0300

    Fix Parameters Issue

--- src/Console/Commands/ScanTranslations.php
+++ src/Console/Commands/ScanTranslations.php
@@ -13,10 +13,9 @@ class ScanTranslations extends Command
     {--locales= : Comma-separated locales, e.g. en,ar}
     {--translate : Enable auto translation via Google API (experimental)}
     {--ignore= : Comma-separated paths to ignore (e.g. views,controllers)}
-    {--ignoreFile= : Comma-separated file names to ignore (e.g. welcome.blade.php,home.blade.php)}
-    {--ignoreDir= : Comma-separated directories to ignore (e.g. vendor,storage,cache)}
+    {--ignore-file= : Comma-separated file names to ignore (e.g. welcome.blade.php,home.blade.php)}
+    {--ignore-dir= : Comma-separated directories to ignore (e.g. vendor,storage,cache)}
     {--overwrite : Overwrite existing translations with new ones}';
-
 
 
     protected $description = 'Scan Blade & PHP files recursively and update translation files';
@@ -32,12 +31,12 @@ class ScanTranslations extends Command
                 ? explode(',', $this->option('ignore'))
                 : [];
 
-            $ignoreFiles = $this->option('ignoreFile')
-                ? array_map('trim', explode(',', $this->option('ignoreFile')))
+            $ignoreFiles = $this->option('ignore-file')
+                ? array_map('trim', explode(',', $this->option('ignore-file')))
                 : [];
 
-            $ignoreDirs = $this->option('ignoreDir')
-                ? array_map('trim', explode(',', $this->option('ignoreDir')))
+            $ignoreDirs = $this->option('ignore-dir')
+                ? array_map('trim', explode(',', $this->option('ignore-dir')))
                 : [];
 
             $locales = $this->option('locales')
